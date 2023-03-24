cc9059acb35ff260f1a92a3d60dd7b95e900d7a3
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-03-24 17:22:09 +0100

    Add: snack 2 done

---
+++ snack2/js/script.js
@@ -0,0 +1,41 @@
+const squadreArray = [
+    {
+      nome: "Alfa",
+      punti: Math.floor((Math.random() * 10) + 1),
+      falliSubiti: Math.floor((Math.random() * 10) + 1)
+  },
+  {
+      nome: 'Beta',
+      punti: Math.floor((Math.random() * 20) + 1),
+      falliSubiti: Math.floor((Math.random() * 10) + 1)
+  },
+  {
+      nome: 'Gamma',
+      punti: Math.floor((Math.random() * 30) + 1),
+      falliSubiti: Math.floor((Math.random() * 10) + 1)
+  },
+  {
+      nome: 'Delta',
+      punti: Math.floor((Math.random() * 40) + 1),
+      falliSubiti: Math.floor((Math.random() * 10) + 1)
+  }
+  ];
+
+const input = prompt("Inserisci il nome della tua squadra");
+squadreArray.push(
+  {
+    nome: input,
+    punti: Math.floor((Math.random() * 50) + 1),
+    falliSubiti: Math.floor((Math.random() * 10) + 1)
+  }
+);
+
+const nuovoArray = squadreArray.map((item)=>{
+  return item.nome +' falli subiti: '+item.falliSubiti;
+});
+
+const messaggio = document.getElementById('result');
+for(let i = 0; i < nuovoArray.length; i++){
+    let teamList = `<li>${nuovoArray[i]}</li>`;
+    messaggio.innerHTML += teamList;
+}
