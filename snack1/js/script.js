634c075750dd160727dfa28586aab6c3eee8ae95
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-03-27 09:49:57 +0200

    Correzione

--- snack1/js/script.js
+++ snack1/js/script.js
@@ -17,17 +17,13 @@ const biciArray = [
   }
 ]
 
-const pesi = biciArray.map((item)=>{
-  return item.peso;
-})
-let pesoMinimo = Math.min(...pesi);
-let messaggio = document.getElementById("result");
-const result = biciArray.filter((item)=>{
-  if(pesoMinimo === item.peso){
-    return item.nome;
+let lightest = biciArray[0];
+biciArray.forEach((element) => {
+  if(element.peso < lightest.peso) {
+    lightest = element;
   }
 });
-let[object] = result;
-console.log(object);
-let{nome, peso} = object;
-messaggio.innerHTML =`La bici più leggera è ${nome} e pesa ${peso} kg.`;
+
+console.log(lightest);
+let messaggio = document.getElementById("result");
+messaggio.innerHTML =`La bici più leggera è ${lightest.nome} e pesa ${lightest.peso} kg.`;
